0a1358faf4fababc786830d783b425ef352143f4
xx0101xx <binson0426@gmail.com>
2025-08-30 20:55:09 +0800

    fix:  Cannot find module @vitejs/plugin-vue

--- maolegemao/vite.config.ts
+++ maolegemao/vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue({
+//       // 支持 Markdown 文件加载
+//       include: [/\.vue$/],
+//     }),
+//   ],
+//     // 入口
+//   // build: {
+//   //   outDir: "maomao",
+//   // },
+// });
+
 export default defineConfig({
-  plugins: [
-    vue({
-      // 支持 Markdown 文件加载
-      include: [/\.vue$/],
-    }),
-  ],
-    // 入口
-  // build: {
-  //   outDir: "maomao",
-  // },
-});
+  plugins: [vue()],
+})
